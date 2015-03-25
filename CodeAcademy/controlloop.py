4ccc960469d64aae01469d8e6bf0774d453d7cd6
yukiecho <yukiecho@users.noreply.github.com>
2015-03-25 17:00:30 -0400

    Update controlloop.py

--- CodeAcademy/controlloop.py
+++ CodeAcademy/controlloop.py
@@ -18,8 +18,11 @@ clinic()
 print 'Welcome to the Pig Latin Translator!'
 
 # Start coding here!
+print 'Welcome to the Pig Latin Translator!'
+
+# Start coding here!
 original = raw_input("Enter a word:")
-if len(original)>0:
+if len(original)>0 and original.isalpha():
     print original
 else:
     print "empty"
