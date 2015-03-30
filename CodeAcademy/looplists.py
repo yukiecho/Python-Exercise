9e9f8b234a99d4c0f5458955c79405cb38fc7bcf
yukiecho <yukiecho@users.noreply.github.com>
2015-03-30 10:55:51 -0400

    Create looplists.py

---
+++ CodeAcademy/looplists.py
@@ -0,0 +1,13 @@
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results = []
+    for listitem in lists:
+        for numbers in listitem:
+            results.append(numbers)
+            
+    return results
+
+
+
+print flatten(n)
