203efb8e1a07c0004881af12fe5731c3e93539a7
yukiecho <yukiecho@users.noreply.github.com>
2015-04-05 15:57:08 -0400

    Update maxindictionary.py

--- Coursera/maxindictionary.py
+++ Coursera/maxindictionary.py
@@ -1,5 +1,5 @@
 #Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail messages. The #program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a #Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the #dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
-
+#https://www.youtube.com/watch?v=3cwXN5_3K6Q&index=25&list=PLlRFEj9H3Oj4JXIwMwN1_ss1Tk8wZShEJ
 name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
