6b01b429c199e39066a0a6788a37d3c6ea5053f2
yukiecho <yukiecho@users.noreply.github.com>
2015-04-06 12:50:32 -0400

    Update practice.py

--- CodeAcademy/practice.py
+++ CodeAcademy/practice.py
@@ -34,3 +34,9 @@ def anti_vowel(text):
         else: tem.append(t)
         
     return "".join(tem)
+
+
+#Use a list comprehension to create a list, threes_and_fives, that consists only of the numbers between 1 and 15 (inclusive) that are evenly divisible by 3 or 5.
+threes_and_fives = [ x for x in range(1,16) if (x%3 ==0 or x%5==0)]
+
+print threes_and_fives
