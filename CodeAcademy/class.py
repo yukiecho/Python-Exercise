7762a8a2105bf36936ea68a25ceb61baf7e26611
yukiecho <yukiecho@users.noreply.github.com>
2015-04-07 11:52:29 -0400

    Update class.py

--- CodeAcademy/class.py
+++ CodeAcademy/class.py
@@ -152,3 +152,23 @@ class Equilateral(Triangle):
         self.angle1 = self.angle
         self.angle2 = self.angle
         self.angle3 = self.angle
+
+
+#good example of understanding python
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+        
+    def display_car(self):
+        print "This is a %s %s with %s MPG." %(self.color,self.model,self.mpg)
+
+my_car = Car("DeLorean", "silver", 88)
+print my_car.condition
+print my_car.model
+print my_car.color
+print my_car.mpg
+
+my_car.display_car()
