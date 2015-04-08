2790f7f412738c492ed61054c0761508bbad72cc
yukiecho <yukiecho@users.noreply.github.com>
2015-04-08 08:51:10 -0400

    Update class.py

--- CodeAcademy/class.py
+++ CodeAcademy/class.py
@@ -235,3 +235,16 @@ print my_car.condition
 my_car.drive_car()
 
 print my_car.condition
+
+# Example of call Build-in function: __repr__()
+class Point3D(object):
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+    def __repr__(self):
+        return ("(%d, %d, %d)" % (self.x, self.y, self.z))
+        
+my_point = Point3D(1,2,3)
+
+print my_point.__repr__()
